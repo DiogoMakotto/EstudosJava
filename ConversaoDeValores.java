5b1dc5f974420218123acab9c4dee8a91d5b1bb1
DiogoMakotto <diogomakotto@gmail.com>
2022-05-14 22:16:05 -0300

    Add Hello wolrd e expressões numéricas

---
+++ ConversaoDeValores.java
@@ -0,0 +1,29 @@
+
+public class ConversaoDeValores {
+    
+	public static void main(String[] args){
+
+        float pontoFlutuante = 3.14f;
+
+        double salario = 1270.50;
+        int valor = (int) salario;
+        System.out.println(valor);
+        
+        //numeros grandes
+        long numeroGrande = 32432423523L;
+        
+        //numeros pequenos
+        short valorPequeno = 2131;
+        
+        //numeros em byte
+        byte b = 127;
+        
+        //famoso valor 0.30000000000000004
+
+        double valor1 = 0.2;
+        double valor2 = 0.1;
+        double total = valor1 + valor2;
+
+        System.out.println(total);
+    }
+}
---
+++ PontosFlutuantes.java
@@ -0,0 +1,20 @@
+
+public class PontosFlutuantes {
+    
+	public static void main(String[] args){
+		
+		//numeros flutuantes
+        double salario = 1350.70;
+
+        System.out.println("Meu salario é " + salario);
+        
+        double divisao = 5.0 / 2;
+        System.out.println("divisao: " + divisao);
+        
+        int meses = 3;
+        double salarioMensal = 1763.50;
+        double multiplicacao = salarioMensal * meses ;
+        System.out.println("Meu salario total de " + meses + " mêses é igual a " + multiplicacao );
+        
+    }
+}
