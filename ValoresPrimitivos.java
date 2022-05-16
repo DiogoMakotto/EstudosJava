84bca3de5d5dd3bb55216089cd0adfcf4cf312b6
DiogoMakotto <diogomakotto@gmail.com>
2022-05-15 23:31:46 -0300

    Testando variaveis primitivas

---
+++ ValoresPrimitivos.java
@@ -0,0 +1,11 @@
+public class ValoresPrimitivos {
+    
+    int primeiro = 5;
+    int segundo = 7;
+    segundo = primeiro;
+    primeiro = 10;
+    
+    // Segundo = 5, pois variavel guarda o valor não a referencia
+    
+    System.out.println(segundo);
+}
