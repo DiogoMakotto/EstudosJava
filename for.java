24ea29d32aabe1e5f22c0f5192426cc769ccefad
DiogoMakotto <diogomakotto@gmail.com>
2022-05-16 13:31:20 -0300

    Metodo for

---
+++ for.java
@@ -0,0 +1,9 @@
+public class for {
+    public static void main(String[] args) {
+        
+        //for utilizado dentro do laço != while
+        for(int contador = 0; contador <= 10; contador++) {
+            System.out.println(contador);
+        }
+    }
+}
