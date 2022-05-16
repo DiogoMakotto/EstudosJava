c77c0dff64a81ccda065f9e51cabec1a239f6f65
DiogoMakotto <diogomakotto@gmail.com>
2022-05-16 13:17:57 -0300

    Add progressao aritmedica

---
+++ ProgressaoAritmedica.java
@@ -0,0 +1,14 @@
+public class ProgressaoAritmedica{
+    public static void main(String[] args) {
+        //var do contador
+        int contador = 0;
+        int total = 0;
+        
+        //soma
+        while(contador <= 10) {
+            total = total + contador;
+            contador++;
+            }
+            System.out.println(total);
+    }
+}
