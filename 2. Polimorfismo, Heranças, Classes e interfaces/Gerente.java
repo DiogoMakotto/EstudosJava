04f820db86e3a8496de4bd9b955c2e36e977ccec
DiogoMakotto <diogomakotto@gmail.com>
2022-06-08 20:35:25 -0300

    Add class Gerente

---
+++ 2. Polimorfismo, Heranças, Classes e interfaces/Gerente.java
@@ -0,0 +1,27 @@
+//extends é utilizado para atribuir uma classe mãe para uma filha
+public class Gerente extends Funcionario{
+	private int senha;
+	
+	public int getSenha() {
+		return senha;
+	}
+
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+
+	//autenticação de senha 
+	public boolean autentica(int senha) {
+        if(this.senha == senha) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //função de bonificação do nosso funcionario do gerente
+    //public double getBonificacao(){
+    //	return this.salario;
+    //}
+    
+}
---
+++ 2. Polimorfismo, Heranças, Classes e interfaces/testeGerente.java
@@ -0,0 +1,16 @@
+public class testeGerente {
+    public static void main(String[] args) {
+        Gerente g1 = new Gerente();
+        g1.setNome("João");
+        g1.setCpf("12345678-9");
+        g1.setSalario(5000);
+
+        System.out.println(g1.getNome());
+        System.out.println(g1.getCpf());
+        System.out.println(g1.getSalario());
+
+        g1.setSenha(4444);
+        boolean autenticou =  g1.autentica(4444);
+        System.out.println(autenticou);
+    }
+}
