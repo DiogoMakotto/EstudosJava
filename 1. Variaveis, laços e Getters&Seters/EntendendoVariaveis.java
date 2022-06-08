9a7f83232c60daaf610ac079e44a594e7443829c
DiogoMakotto <diogomakotto@gmail.com>
2022-06-08 17:43:30 -0300

    fix reformulando pastas

---
+++ 1. Variaveis, laços e Getters&Seters/EntendendoVariaveis.java
@@ -0,0 +1,11 @@
+
+public class EntendendoVariaveis {
+	
+	public static void main(String[] args) {
+		
+		//int para numeros inteiros
+		int idade = 21;
+
+		System.out.println("A idade de Diogo é " + idade);
+	}
+}
