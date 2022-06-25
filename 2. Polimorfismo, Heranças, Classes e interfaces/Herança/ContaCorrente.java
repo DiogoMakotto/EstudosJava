fed7e315d03aaaefbe7351a0eb4bbdcd09b88e1d
DiogoMakotto <diogomakotto@gmail.com>
2022-06-25 20:23:13 -0300

    modificando classes da conta

--- 2. Polimorfismo, Heranças, Classes e interfaces/Herança/ContaCorrente.java
+++ 2. Polimorfismo, Heranças, Classes e interfaces/Herança/ContaCorrente.java
@@ -1,15 +1,20 @@
 package Herança;
 
 //new ContaCorrente();
-public class ContaCorrente extends Conta{
-    
-    public ContaCorrente(int agencia, int numero){
+public class ContaCorrente extends Conta {
+
+    public ContaCorrente(int agencia, int numero) {
         super(agencia, numero);
     }
 
     @Override
     public boolean saca(double valor) {
-        double valorAsacar = valor + 0.2; //testando polimorfismo com uma taxa de 0.2 por saque
+        double valorAsacar = valor + 0.2; // testando polimorfismo com uma taxa de 0.2 por saque
         return super.saca(valorAsacar);
+    }
+
+    @Override
+    public void deposita(double valor) {
+        this.saldo += valor;
     }
 }
