a808d45802c00fdb1670132e8fdc76684452f965
DiogoMakotto <diogomakotto@gmail.com>
2022-06-29 18:31:05 -0300

    add anotações em Java

---
+++ 2. Polimorfismo, Heranças, Classes e interfaces/Anotações/testeAnotacoes.java
@@ -0,0 +1,28 @@
+package Anotações;
+
+public class testeAnotacoes {
+
+    //anotações servem para reduzir o uso do xml 
+    /**
+     * @author Diogo Makotto
+     * 
+     * @version 1.0
+     * 
+     *  @Entity  utilizada para informar que uma classe também é uma entidade
+     * 
+     *  @id utilizado como identificador
+     * 
+     *  @Table tabela relacionada no bd
+     * 
+     *  @Override é anotação para o compilador, para verificar as regras dos metodos gerados
+     * 
+     *  Para criar as próprias anotações se cria uma interface com @(Arroba) na frente 
+     * 
+     *  @Target é utilizado na criação de anotações para definir o tipo (ex. ElementType.FIELD ou METHOD)
+     * 
+     *  @Retention define onde o valor vai ser utilizado, se é uma chave primaria ou secundaria 
+     * 
+     *  
+     */
+   
+}
