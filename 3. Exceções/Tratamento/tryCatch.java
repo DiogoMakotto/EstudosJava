ac273da232531cf9395a644724d3dda357cff989
DiogoMakotto <diogomakotto@gmail.com>
2022-06-30 11:50:07 -0300

    add tryCatch

---
+++ 3. Exceções/Tratamento/tryCatch.java
@@ -0,0 +1,27 @@
+package Tratamento;
+public class tryCatch {
+    public static void main(String[] args) {
+        System.out.println("Ini do main");
+        metodo1();
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+        try{
+            metodo2();   
+        } catch(ArithmeticException ex) {
+            System.out.println("ArithmeticException");
+        }
+        System.out.println("Fim do metodo1");
+    }
+           
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            int a = i /0; //erro proposital para chamar o java.lang.ArithmeticException
+        System.out.println("Fim do metodo2");
+        }
+    }
+}
