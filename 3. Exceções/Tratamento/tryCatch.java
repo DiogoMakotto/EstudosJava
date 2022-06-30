dd68794f64df873adbe8954a8347a9e724e03120
DiogoMakotto <diogomakotto@gmail.com>
2022-06-30 12:11:51 -0300

    reafctor Variação do Catch

--- 3. Exceções/Tratamento/tryCatch.java
+++ 3. Exceções/Tratamento/tryCatch.java
@@ -2,17 +2,20 @@ package Tratamento;
 public class tryCatch {
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        try{
+            metodo1();
+        } catch(ArithmeticException | NullPointerException ex) {
+            String msg = ex.getMessage();
+            System.out.println("Exception " + msg);
+
+            ex.printStackTrace(); //Mostra o rastro do erro 
+        } 
         System.out.println("Fim do main");
     }
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
-        try{
             metodo2();   
-        } catch(ArithmeticException ex) {
-            System.out.println("ArithmeticException");
-        }
         System.out.println("Fim do metodo1");
     }
            
@@ -21,6 +24,10 @@ public class tryCatch {
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
             int a = i /0; //erro proposital para chamar o java.lang.ArithmeticException
+
+            //exemplo e = null;   //erro proposital para chamar ojava.lang.NullPointerException:
+            //e.teste();
+
         System.out.println("Fim do metodo2");
         }
     }
