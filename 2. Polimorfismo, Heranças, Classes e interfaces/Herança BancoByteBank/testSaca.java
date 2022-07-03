6f873f344ce5efba5d1791ea43701de441acc58c
DiogoMakotto <diogomakotto@gmail.com>
2022-07-03 17:48:05 -0300

    add teste class

---
+++ 2. Polimorfismo, Heranças, Classes e interfaces/Herança BancoByteBank/testSaca.java
@@ -0,0 +1,15 @@
+import javax.security.sasl.SaslException;
+
+public class testSaca {
+    public static void main(String[] args) {
+        Conta conta = new ContaCorrente(123, 321);
+
+        conta.deposita(200.0);
+        try{
+            conta.saca(210.0); //chamando a exception de saldo insuficiente por conta da falta de saldo
+        }catch(Exception ex){
+            System.out.println("Ex: " + ex.getMessage());       
+        }
+        System.out.println(conta.getSaldo());
+    }
+}
