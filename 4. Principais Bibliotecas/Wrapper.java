a19cd419046ad3db7d1b6e30e2bc3dff74b43cb9
DiogoMakotto <diogomakotto@gmail.com>
2022-07-15 22:42:41 -0300

    add Wrapper

---
+++ 4. Principais Bibliotecas/Wrapper.java
@@ -0,0 +1,12 @@
+package br.com.TestandoBibliotecas;
+
+public class Wrapper { //Criando Objetos Wrapper
+    
+    Integer myInt = 5;
+    Double myDouble = 5.99;
+    Character myChar = 'A';
+    System.out.println(myInt);
+    System.out.println(myDouble);
+    System.out.println(myChar);
+  }
+}
